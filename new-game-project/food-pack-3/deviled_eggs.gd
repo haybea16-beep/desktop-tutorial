9e81229242e2e08b6d8d960b3336c79d1194719a
haybea16-beep <haybea16@kingshigh.school.nz>
2026-06-24 15:01:00 +1200

    24/6

--- new-game-project/food-pack-3/deviled_eggs.gd
+++ new-game-project/food-pack-3/deviled_eggs.gd
@@ -6,5 +6,13 @@ var gravity = 300.0
 func _ready():
 	$wholesprite/AnimationPlayer.play("RESET")
 	
-func 
- d
+func _process(delta):
+	rotation_degrees += 120 * delta
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	move_and_slide()
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is
